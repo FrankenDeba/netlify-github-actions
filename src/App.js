8fac11b7daa9d25cc7b7bac6a40cbfaf846c4564
FrankenDeba <debashishrambhola@gmail.com>
2022-12-24 17:35:32 +0530

    modifying app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [welcome, setWelcome] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React app deployed to netlify with github actions ci/cd</h1>
+      <button onClick={() => setWelcome(true)}>Say thank you</button>
+      {welcome && <h2>You are welcome!</h2>}
     </div>
   );
 }
