2d8a3dc1b2670b3f4b2cf0e84fde9b07c9823811
FrankenDeba <debashishrambhola@gmail.com>
2022-12-24 17:38:20 +0530

    fixing cypress url

--- cypress/e2e/specOne.cy.js
+++ cypress/e2e/specOne.cy.js
@@ -1,6 +1,6 @@
 describe("First cypress test", () => {
   it("Should visit the page", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit("/");
     cy.contains("React app deployed to netlify with github actions ci/cd");
   });
 });
--- cypress/e2e/specTwo.cy.js
+++ cypress/e2e/specTwo.cy.js
@@ -1,6 +1,6 @@
 describe("Testing welcome", () => {
   it("Should greet back", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit("/");
     cy.contains("Say thank you").click();
     cy.contains("You are welcome!");
   });
