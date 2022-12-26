0115d2ad095b7b141d9ef44b4f08a265de1bdc0b
FrankenDeba <debashishrambhola@gmail.com>
2022-12-26 19:11:54 +0530

    remove codex

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
       <h1>React app deployed to netlify with github actions ci/cd</h1>
       <button onClick={() => setWelcome(true)}>Say thank you</button>
       {welcome && <h2>You are welcome!</h2>}
-      <p>added some extra code</p>
     </div>
   );
 }
